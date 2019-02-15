73c0ed87ab1b785262e2b48dde96021f79b6682f
TobiasLrsen <tobiash.larsen@outlook.dk>
2019-02-15 11:06:18 +0100

    sdf

--- public_html/index.php
+++ public_html/index.php
@@ -71,7 +71,7 @@
 
         ?>" /> 
 
-    <iframe width="100%" height="100%" src="<?php echo $rowMedieData['vcFile'] ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    <iframe width="100%" height="100%" src="<?php echo $rowMedieData['vcFile'] ?>?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
 
 
 
